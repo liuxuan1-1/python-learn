5c08b5b88b5fa2f992470613317563a1cf5cffef
liuxuan <2466959245@qq.com>
2024-06-04 22:52:00 +0800

    feat: add alien ships

--- alien_invasion_game/alien_invasion.py
+++ alien_invasion_game/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +24,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         print("Game is running")
@@ -31,8 +35,29 @@ class AlienInvasion:
             self.ship.update()
             # When you call update() on a group, the group automatically calls update() for each sprite in the group.
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
+
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _update_bullets(self):
         # When you call update() on a group, the group automatically calls update() for each sprite in the group.
@@ -40,6 +65,12 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _check_events(self):
         # This function returns a list of events that have taken place since the last time this function was called
@@ -67,6 +98,17 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _fire_bullet(self):
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
@@ -77,6 +119,7 @@ class AlienInvasion:
             bullet.draw_bullet()
         # This function draws the ship on the screen
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         # This function updates the display and continuously updates the display
         pygame.display.flip()
