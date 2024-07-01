27a32c0c65af99e9eff3a7231dbc9b481820098a
liuxuan <2466959245@qq.com>
2024-07-01 23:47:25 +0800

    feat: add tool

--- langgraph/main.py
+++ langgraph/main.py
@@ -2,10 +2,42 @@ from dotenv import load_dotenv
 from typing import Annotated
 
 from typing_extensions import TypedDict
+from typing import Literal
 
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+import json
+
+from langchain_core.messages import ToolMessage, BaseMessage
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+
 
 load_dotenv()
 
@@ -17,23 +49,54 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
+def route_tools(
+    state: State,
+) -> Literal["tools", "__end__"]:
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return "__end__"
+
+
 graph_builder = StateGraph(State)
 llm = ChatOpenAI(
     model="gpt-4o",
     temperature=0,
 )
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+llm_with_tools = llm.bind_tools(tools)
 
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
 # The first argument is the unique node name
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
+tool_node = BasicToolNode(tools)
+graph_builder.add_node("tools", tool_node)
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    {
+        "tools": "tools",
+        "__end__": "__end__",
+    },
+)
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.set_entry_point("chatbot")
-graph_builder.set_finish_point("chatbot")
 graph = graph_builder.compile()
 
 
@@ -51,6 +114,7 @@ while True:
     if user_input.lower() in ["quit", "exit", "q"]:
         print("Goodbye!")
         break
-    for event in graph.stream({"messages": ("user", user_input)}):
+    for event in graph.stream({"messages": [("user", user_input)]}):
         for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+            if isinstance(value["messages"][-1], BaseMessage):
+                print("Assistant:", value["messages"][-1].content)
