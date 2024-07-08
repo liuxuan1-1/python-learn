cff1e09fcebb8c7d0fe92c7d143775e218a42a2b
liuxuan <2466959245@qq.com>
2024-07-08 23:43:51 +0800

    feat: add memory

--- langgraph/main.py
+++ langgraph/main.py
@@ -1,42 +1,13 @@
 from dotenv import load_dotenv
 from typing import Annotated
-
 from typing_extensions import TypedDict
-from typing import Literal
 
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
-import json
-
-from langchain_core.messages import ToolMessage, BaseMessage
-
-
-class BasicToolNode:
-    """A node that runs the tools requested in the last AIMessage."""
-
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name=tool_call["name"],
-                    tool_call_id=tool_call["id"],
-                )
-            )
-        return {"messages": outputs}
 
 
 load_dotenv()
@@ -49,24 +20,7 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-def route_tools(
-    state: State,
-) -> Literal["tools", "__end__"]:
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return "__end__"
-
-
+memory = SqliteSaver.from_conn_string(":memory:")
 graph_builder = StateGraph(State)
 llm = ChatOpenAI(
     model="gpt-4o",
@@ -85,19 +39,15 @@ def chatbot(state: State):
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
-tool_node = BasicToolNode(tools)
+tool_node = ToolNode(tools)
 graph_builder.add_node("tools", tool_node)
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    {
-        "tools": "tools",
-        "__end__": "__end__",
-    },
+    tools_condition
 )
 graph_builder.add_edge("tools", "chatbot")
-graph_builder.set_entry_point("chatbot")
-graph = graph_builder.compile()
+graph_builder.add_edge(START, "chatbot")
+graph = graph_builder.compile(checkpointer=memory)
 
 
 # We can use this to generate a graph of the state machine
@@ -109,12 +59,16 @@ graph = graph_builder.compile()
 #     # This requires some extra dependencies and is optional
 #     pass
 
+config = {"configurable": {"thread_id": "1"}}
 while True:
     user_input = input("User: ")
     if user_input.lower() in ["quit", "exit", "q"]:
         print("Goodbye!")
         break
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            if isinstance(value["messages"][-1], BaseMessage):
-                print("Assistant:", value["messages"][-1].content or value["messages"][-1].tool_calls)
+    for event in graph.stream(
+        {"messages": [("user", user_input)]}, config, stream_mode="values"
+    ):
+        event["messages"][-1].pretty_print()
+        # for value in event.values():
+            # if isinstance(value["messages"][-1], BaseMessage):
+            #     print("Assistant:", value["messages"][-1].content or value["messages"][-1].tool_calls)
--- langgraph/tool_node.py
+++ langgraph/tool_node.py
@@ -1,4 +1,6 @@
 import json
+from typing import Literal
+
 
 from langchain_core.messages import ToolMessage
 
@@ -23,3 +25,21 @@ class BasicToolNode:
                 )
             )
         return {"messages": outputs}
+
+
+def route_tools(
+    state,
+) -> Literal["tools", "__end__"]:
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return "__end__"
